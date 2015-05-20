665641cf80f809273626fbb9118d31aad6c857f3
chootka <com@chootka.com>
2015-05-20 13:07:50 -0400

    trying child_process now

--- app/server/server.js
+++ app/server/server.js
@@ -7,7 +7,8 @@ var path = require('path')
     ,cookieParser = require('cookie-parser')
     ,bodyParser = require('body-parser')
     ,http = require('http').createServer(app)
-    ,smb2 = require('smb2');
+    // ,smb2 = require('smb2');
+    ,cp = require('child_process');
 
 
 // -----------------
@@ -33,23 +34,33 @@ app.use(function (req, res, next) {
 // ----------------------------
 // Create an SMB2 instance
 // ----------------------------
-var smb2Client = new smb2({
-  share:'\\\\192.168.3.1\\anonymous'
-, domain:'WORKGROUP'
-, username:''
-, password:''
-, debug: true
-, autoCloseTimeout: 0
-});
+// var smb2Client = new smb2({
+//   share:'\\\\192.168.3.1\\anonymous'
+// , domain:'WORKGROUP'
+// , username:''
+// , password:''
+// , debug: true
+// , autoCloseTimeout: 0
+// });
 
-smb2Client.readdir('\\', function(err, files){
-	if(err) {
-        console.log("Error (readdir):\n", err);
-        console.log("files", files);
-    } else {
-        console.log("Connection made.");
-        console.log(files); 
-    }
+// smb2Client.readdir('\\', function(err, files){
+// 	if(err) {
+//         console.log("Error (readdir):\n", err);
+//         console.log("files", files);
+//     } else {
+//         console.log("Connection made.");
+//         console.log(files); 
+//     }
+// });
+
+var smb = cp.spawn('mount', ['--stdout', '//192.168.3.1/anonymous /mnt/public']);
+smb.stdout.on('data', function(data) {
+    //handle chunk of data
+    console.log("data: " + data);
+});
+smb.on('exit', function() {
+	console.log("smb exit");
+    //file loaded completely, continue doing stuff
 });
 
 
