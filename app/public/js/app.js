050d7f01b0f601fff458bddc70098e28bb81db08
chootka <com@chootka.com>
2015-05-21 12:34:38 -0400

    updated bundle.js

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -9,30 +9,30 @@ module.exports = {
     // this is the whole app init
     init: function () {
 
-        console.log("init: " + init);
+        console.log("init");
 
         var self = window.app = this;
 
         // wait for document ready to render our main view
         domReady(function () {
 
-            // console.log("domReady");
+            console.log("domReady");
 
-            // var $table = $(".linksholder").dataTable();
+            var $table = $(".linksholder").dataTable();
 
-            // console.log("$table.size: " + $table.size());
+            console.log("$table.size: " + $table.size());
 
-            // $.get( "/files", function( data ) {
-            //     console.log("data: " + data);
-            //     for (var i=0; i<data.length; i++) {
-            //         console.log(data[i].name);
-            //         console.log(data[i].path);
-            //         console.log(data[i].ext);
-            //         console.log(data[i].isDir);
-            //     }
-            //     $table.fnClearTable();
-            //     $table.fnAddData(data);
-            // });
+            $.get( "/files", function( data ) {
+                console.log("data: " + data);
+                for (var i=0; i<data.length; i++) {
+                    console.log(data[i].name);
+                    console.log(data[i].path);
+                    console.log(data[i].ext);
+                    console.log(data[i].isDir);
+                }
+                $table.fnClearTable();
+                $table.fnAddData(data);
+            });
 
             // // init our main view
             // var mainView = self.view = new MainView({
